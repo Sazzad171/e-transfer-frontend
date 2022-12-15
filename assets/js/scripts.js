5226525cd06547865985abe0de1028cc82cbb34e
Sazzad171 <sajjad.cse.diu@gmail.com>
2022-12-15 17:14:12 +0600

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,44 @@
+// home banner
+$('.home-banner').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplaySpeed: 1000,
+    dots:false,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+// sticky navbar
+if ($(window).width() > 767) {
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll > 200) {
+            $('.header-bottom-area').addClass("fixed");
+        }
+        else {
+            $('.header-bottom-area').removeClass("fixed");
+        }
+    });
+}
+
+
+if ($(window).width() < 767) {
+    $('.header-bottom-area').addClass("fixed");
+    $('body').css("margin-top", "85px")
+}
